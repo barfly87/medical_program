f1d39db93a9c909ef17aaafae5ba5ec697d38610
barfly87 <chrivai@yahoo.gr>
2015-03-31 10:41:08 +0200

    Added description of this file's usability

--- db/usyd/data_lookup_table_clinical_school.sql
+++ db/usyd/data_lookup_table_clinical_school.sql
@@ -1,3 +1,4 @@
+-- Used for evaluating teaching activities (few types)
 INSERT INTO clinical_school VALUES (DEFAULT,  'Northern');
 INSERT INTO clinical_school VALUES (DEFAULT,  'Central');
 INSERT INTO clinical_school VALUES (DEFAULT,  'Concord');
